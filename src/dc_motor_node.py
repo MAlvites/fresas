f3c8686034726527abb604917c88af37ce344c02
MAlvites <malvites1602@gmail.com>
2024-04-25 14:59:53 -0500

    Relative position topic and zero set service added

--- src/dc_motor_node.py
+++ src/dc_motor_node.py
@@ -6,6 +6,7 @@ import argparse
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int32
+from std_srvs.srv import Empty
 
 class DCMotorNode(Node):
     def __init__(self, serial_port):
@@ -14,11 +15,22 @@ class DCMotorNode(Node):
         self.serial_port_name = serial_port
         self.init_serial()
 
-        self.subscription = self.create_subscription(
+        self.position_subscription = self.create_subscription(
             Int32,
             'dc_motor_1/position',
             self.position_callback,
             10)
+        
+        self.rel_position_subscription = self.create_subscription(
+            Int32,
+            'dc_motor_1/rel_position',
+            self.rel_position_callback,
+            10)
+        
+        self.zero_set_service = self.create_service(
+            Empty,
+            'dc_motor_1/zero_set',
+            self.zero_set_callback)
 
     def init_serial(self):
         try:
@@ -43,12 +55,32 @@ class DCMotorNode(Node):
         except serial.SerialException as e:
             self.get_logger().error('Error communicating with motor: %s' % str(e))
 
-def main(args=None):
+    def rel_position_callback(self, msg):
+        try:
+            rel_position = msg.data
+            command = "N24 P{} v100/n".format(rel_position)
+            self.serial_port.write(command.encode())
+        except serial.SerialException as e:
+            self.get_logger().error('Error communicating with motor: %s' % str(e))
+
+    def zero_set_callback(self, request, response):
+        try:
+            # Enviar comando al motor para redefinir cero
+            self.serial_port.write("N24 O r".encode())
+            # Publicar respuesta
+            self.get_logger().info('Zero set')
+            return response
+        except serial.SerialException as e:
+            self.get_logger().error('Error communicating with motor: %s' % str(e))
+            return response
+
+
+def main():
     parser = argparse.ArgumentParser(description='DC Motor Node')
-    parser.add_argument('--serial-port', dest='serial_port', type=str, required=True, help='Serial port name')
+    parser.add_argument('--serial-port', dest='serial_port', type=str, default='/dev/ttyACM0', help='Serial port name (default: /dev/ttyACM0)')
     args = parser.parse_args()
 
-    rclpy.init(args=args)
+    rclpy.init(args=None)
     try:
         node = DCMotorNode(args.serial_port)
         rclpy.spin(node)
