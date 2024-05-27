e35bd6dcae24e7917e516c4fe2bfecf9de1ccd8e
Marcelo <malvites1602@gmail.com>
2024-05-27 17:35:38 -0500

    CMakeList updated

--- src/can_split_message.py
+++ src/can_split_message.py
@@ -49,28 +49,28 @@ class CANHandler(Node):
         self.publishers = {}
 
         for motor in motor_ids.values():
-            self.publishers[f'{motor}_status_rpm'] = self.create_publisher(Int32, f'{motor}_status_rpm', 10)
-            self.publishers[f'{motor}_status_current'] = self.create_publisher(Float32, f'{motor}_status_current', 10)
-            self.publishers[f'{motor}_status_duty'] = self.create_publisher(Float32, f'{motor}_status_duty', 10)
+            self.publishers[f'{motor}_rpm'] = self.create_publisher(Int32, f'{motor}_rpm', 10)
+            self.publishers[f'{motor}_current'] = self.create_publisher(Float32, f'{motor}_current', 10)
+            self.publishers[f'{motor}_duty'] = self.create_publisher(Float32, f'{motor}_duty', 10)
 
-            self.publishers[f'{motor}_status2_amp_hours'] = self.create_publisher(Float32, f'{motor}_status2_amp_hours', 10)
-            self.publishers[f'{motor}_status2_amp_hours_chg'] = self.create_publisher(Float32, f'{motor}_status2_amp_hours_chg', 10)
+            self.publishers[f'{motor}_amp_hours'] = self.create_publisher(Float32, f'{motor}_amp_hours', 10)
+            self.publishers[f'{motor}_amp_hours_chg'] = self.create_publisher(Float32, f'{motor}_amp_hours_chg', 10)
 
-            self.publishers[f'{motor}_status3_watt_hours'] = self.create_publisher(Float32, f'{motor}_status3_watt_hours', 10)
-            self.publishers[f'{motor}_status3_watt_hours_chg'] = self.create_publisher(Float32, f'{motor}_status3_watt_hours_chg', 10)
+            self.publishers[f'{motor}_watt_hours'] = self.create_publisher(Float32, f'{motor}_watt_hours', 10)
+            self.publishers[f'{motor}_watt_hours_chg'] = self.create_publisher(Float32, f'{motor}_watt_hours_chg', 10)
 
-            self.publishers[f'{motor}_status4_temp_fet'] = self.create_publisher(Float32, f'{motor}_status4_temp_fet', 10)
-            self.publishers[f'{motor}_status4_temp_motor'] = self.create_publisher(Float32, f'{motor}_status4_temp_motor', 10)
-            self.publishers[f'{motor}_status4_current_input'] = self.create_publisher(Float32, f'{motor}_status4_current_input', 10)
-            self.publishers[f'{motor}_status4_pid_pos'] = self.create_publisher(Float32, f'{motor}_status4_pid_pos', 10)
+            self.publishers[f'{motor}_temp_fet'] = self.create_publisher(Float32, f'{motor}_temp_fet', 10)
+            self.publishers[f'{motor}_temp_motor'] = self.create_publisher(Float32, f'{motor}_temp_motor', 10)
+            self.publishers[f'{motor}_current_input'] = self.create_publisher(Float32, f'{motor}_current_input', 10)
+            self.publishers[f'{motor}_pid_pos'] = self.create_publisher(Float32, f'{motor}_pid_pos', 10)
 
-            self.publishers[f'{motor}_status5_tachometer'] = self.create_publisher(Float32, f'{motor}_status5_tachometer', 10)
-            self.publishers[f'{motor}_status5_volts_input'] = self.create_publisher(Float32, f'{motor}_status5_volts_input', 10)
+            self.publishers[f'{motor}_tachometer'] = self.create_publisher(Float32, f'{motor}_tachometer', 10)
+            self.publishers[f'{motor}_volts_input'] = self.create_publisher(Float32, f'{motor}_volts_input', 10)
 
-            self.publishers[f'{motor}_status6_adc_1'] = self.create_publisher(Float32, f'{motor}_status6_adc_1', 10)
-            self.publishers[f'{motor}_status6_adc_2'] = self.create_publisher(Float32, f'{motor}_status6_adc_2', 10)
-            self.publishers[f'{motor}_status6_adc_3'] = self.create_publisher(Float32, f'{motor}_status6_adc_3', 10)
-            self.publishers[f'{motor}_status6_ppm'] = self.create_publisher(Float32, f'{motor}_status6_ppm', 10)
+            self.publishers[f'{motor}_adc_1'] = self.create_publisher(Float32, f'{motor}_adc_1', 10)
+            self.publishers[f'{motor}_adc_2'] = self.create_publisher(Float32, f'{motor}_adc_2', 10)
+            self.publishers[f'{motor}_adc_3'] = self.create_publisher(Float32, f'{motor}_adc_3', 10)
+            self.publishers[f'{motor}_ppm'] = self.create_publisher(Float32, f'{motor}_ppm', 10)
 
     def listener_callback(self, msg):
         can_msg = msg.data
@@ -100,49 +100,49 @@ class CANHandler(Node):
             motor = motor_ids[motor_id]
             if command_id == CAN_PACKET_STATUS:
                 rpm = int.from_bytes(can_data[0:3], "big", signed=True)
-                current = int.from_bytes(can_data[4:5], "big", signed=True) / 10
-                duty = int.from_bytes(can_data[6:7], "big", signed=True) / 1000
-                self.publishers[f'{motor}_status_rpm'].publish(Int32(data=rpm))
-                self.publishers[f'{motor}_status_current'].publish(Float32(data=current))
-                self.publishers[f'{motor}_status_duty'].publish(Float32(data=duty))
+                current = int.from_bytes(can_data[4:5], "big", signed=True) / 10.0
+                duty = int.from_bytes(can_data[6:7], "big", signed=True) / 1000.0
+                self.publishers[f'{motor}_rpm'].publish(Int32(data=rpm))
+                self.publishers[f'{motor}_current'].publish(Float32(data=current))
+                self.publishers[f'{motor}_duty'].publish(Float32(data=duty))
 
             elif command_id == CAN_PACKET_STATUS_2:
-                amp_hours = int.from_bytes(can_data[0:3], "big", signed=True) / 10000
-                amp_hours_chg = int.from_bytes(can_data[4:7], "big", signed=True) / 10000
-                self.publishers[f'{motor}_status2_amp_hours'].publish(Float32(data=amp_hours))
-                self.publishers[f'{motor}_status2_amp_hours_chg'].publish(Float32(data=amp_hours_chg))
+                amp_hours = int.from_bytes(can_data[0:3], "big", signed=True) / 10000.0
+                amp_hours_chg = int.from_bytes(can_data[4:7], "big", signed=True) / 10000.0
+                self.publishers[f'{motor}_amp_hours'].publish(Float32(data=amp_hours))
+                self.publishers[f'{motor}_amp_hours_chg'].publish(Float32(data=amp_hours_chg))
 
             elif command_id == CAN_PACKET_STATUS_3:
-                watt_hours = int.from_bytes(can_data[0:3], "big", signed=True) / 10000
-                watt_hours_chg = int.from_bytes(can_data[4:7], "big", signed=True) / 10000
-                self.publishers[f'{motor}_status3_watt_hours'].publish(Float32(data=watt_hours))
-                self.publishers[f'{motor}_status3_watt_hours_chg'].publish(Float32(data=watt_hours_chg))
+                watt_hours = int.from_bytes(can_data[0:3], "big", signed=True) / 10000.0
+                watt_hours_chg = int.from_bytes(can_data[4:7], "big", signed=True) / 10000.0
+                self.publishers[f'{motor}_watt_hours'].publish(Float32(data=watt_hours))
+                self.publishers[f'{motor}_watt_hours_chg'].publish(Float32(data=watt_hours_chg))
 
             elif command_id == CAN_PACKET_STATUS_4:
-                temp_fet = int.from_bytes(can_data[0:1], "big", signed=True) / 10
-                temp_motor = int.from_bytes(can_data[2:3], "big", signed=True) / 10
-                current_input = int.from_bytes(can_data[4:5], "big", signed=True) / 10
-                pid_pos = int.from_bytes(can_data[6:7], "big", signed=True) / 50
-                self.publishers[f'{motor}_status4_temp_fet'].publish(Float32(data=temp_fet))
-                self.publishers[f'{motor}_status4_temp_motor'].publish(Float32(data=temp_motor))
-                self.publishers[f'{motor}_status4_current_input'].publish(Float32(data=current_input))
-                self.publishers[f'{motor}_status4_pid_pos'].publish(Float32(data=pid_pos))
+                temp_fet = int.from_bytes(can_data[0:1], "big", signed=True) / 10.0
+                temp_motor = int.from_bytes(can_data[2:3], "big", signed=True) / 10.0
+                current_input = int.from_bytes(can_data[4:5], "big", signed=True) / 10.0
+                pid_pos = int.from_bytes(can_data[6:7], "big", signed=True) / 50.0
+                self.publishers[f'{motor}_temp_fet'].publish(Float32(data=temp_fet))
+                self.publishers[f'{motor}_temp_motor'].publish(Float32(data=temp_motor))
+                self.publishers[f'{motor}_current_input'].publish(Float32(data=current_input))
+                self.publishers[f'{motor}_pid_pos'].publish(Float32(data=pid_pos))
 
             elif command_id == CAN_PACKET_STATUS_5:
-                tachometer = int.from_bytes(can_data[0:3], "big", signed=True) / 6
-                volts_input = int.from_bytes(can_data[4:5], "big", signed=True) / 10
-                self.publishers[f'{motor}_status5_tachometer'].publish(Float32(data=tachometer))
-                self.publishers[f'{motor}_status5_volts_input'].publish(Float32(data=volts_input))
+                tachometer = int.from_bytes(can_data[0:3], "big", signed=True) / 6.0
+                volts_input = int.from_bytes(can_data[4:5], "big", signed=True) / 10.0
+                self.publishers[f'{motor}_tachometer'].publish(Float32(data=tachometer))
+                self.publishers[f'{motor}_volts_input'].publish(Float32(data=volts_input))
 
             elif command_id == CAN_PACKET_STATUS_6:
-                adc_1 = int.from_bytes(can_data[0:1], "big", signed=True) / 1000
-                adc_2 = int.from_bytes(can_data[2:3], "big", signed=True) / 1000
-                adc_3 = int.from_bytes(can_data[4:5], "big", signed=True) / 1000
-                ppm = int.from_bytes(can_data[6:7], "big", signed=True) / 1000
-                self.publishers[f'{motor}_status6_adc_1'].publish(Float32(data=adc_1))
-                self.publishers[f'{motor}_status6_adc_2'].publish(Float32(data=adc_2))
-                self.publishers[f'{motor}_status6_adc_3'].publish(Float32(data=adc_3))
-                self.publishers[f'{motor}_status6_ppm'].publish(Float32(data=ppm))
+                adc_1 = int.from_bytes(can_data[0:1], "big", signed=True) / 1000.0
+                adc_2 = int.from_bytes(can_data[2:3], "big", signed=True) / 1000.0
+                adc_3 = int.from_bytes(can_data[4:5], "big", signed=True) / 1000.0
+                ppm = int.from_bytes(can_data[6:7], "big", signed=True) / 1000.0
+                self.publishers[f'{motor}_adc_1'].publish(Float32(data=adc_1))
+                self.publishers[f'{motor}_adc_2'].publish(Float32(data=adc_2))
+                self.publishers[f'{motor}_adc_3'].publish(Float32(data=adc_3))
+                self.publishers[f'{motor}_ppm'].publish(Float32(data=ppm))
             else:
                 self.get_logger().warning("Command Id not identified")
         else:
