b0efd9c71acd65056923bd296a355fd8cd3ea4b6
MAlvites <malvites1602@gmail.com>
2024-10-14 15:51:26 -0500

    Fixed multiple bldc motors data saving

--- src/data_collector.py
+++ src/data_collector.py
@@ -7,14 +7,40 @@ from std_srvs.srv import Empty
 import csv
 import time
 
+MOTOR_ID_1 = 0x0042
+MOTOR_ID_2 = 0x0057
+MOTOR_ID_3 = 0x0001 
+MOTOR_ID_4 = 0x0072
+
+CAN_PACKET_STATUS = 9
+CAN_PACKET_STATUS_2 = 14
+CAN_PACKET_STATUS_3 = 15
+CAN_PACKET_STATUS_4 = 16
+CAN_PACKET_STATUS_5 = 27
+CAN_PACKET_STATUS_6 = 28
+
+motor_ids = {
+    MOTOR_ID_1: "motor_front_left",
+    MOTOR_ID_2: "motor_front_right",
+    MOTOR_ID_3: "motor_back_left",
+    MOTOR_ID_4: "motor_back_right",
+}
+
+command_ids = {
+    CAN_PACKET_STATUS: "ERPM, Current, Duty Cycle",
+    #CAN_PACKET_STATUS_2: "Ah Used, Ah Charged",
+    #CAN_PACKET_STATUS_3: "Wh Used, Wh Charged",
+    #CAN_PACKET_STATUS_4: "Temp Fet, Temp Motor, Current In, PID position",
+    CAN_PACKET_STATUS_5: "Tachometer, Voltage In",
+    #CAN_PACKET_STATUS_6: "ADC1, ADC2, ADC3, PPM",
+}
+
 class DataCollector(Node):
     def __init__(self):
         super().__init__('data_collector')
 
-        # Lists to store timestamps and data
-        self.time_stamps = []
-        self.rpm_setpoint = []
-        self.can_data = []
+        # List to store all recorded data as dictionaries
+        self.data_entries = []
 
         # Recording flag
         self.record_flag = False
@@ -25,7 +51,7 @@ class DataCollector(Node):
 
         # Initialize the file with headers
         with open(self.csv_filename, 'w', newline='') as csvfile:
-            fieldnames = ['timestamp', 'rpm_setpoint', 'rpm', 'current', 'battery']
+            fieldnames = ['timestamp', 'motor_id', 'rpm_setpoint', 'rpm', 'current', 'battery']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
 
@@ -54,16 +80,7 @@ class DataCollector(Node):
         try:
             current_time = time.time()
             if self.record_flag:
-                self.get_logger().info("Recording CAN data")
-                rpm_can = extract_rpm_can(msg.data)
-                current_can = extract_current_can(msg.data)
-                battery_can = extract_battery_can(msg.data)
-                if rpm_can != "nan" or current_can != "nan" or battery_can != "nan":
-                    self.rpm_setpoint.append("nan")
-                    self.can_data.append((rpm_can, current_can, battery_can))
-                    self.time_stamps.append(current_time)
-                    self.get_logger().info(f"Received CAN message at {current_time}: RPM={rpm_can}, Current={current_can}, Battery={battery_can}")
-                self.save_data()  # Save data after processing CAN message
+                self.save_can_information(msg.data, current_time)
         except Exception as e:
             self.get_logger().error(f"Error in CAN callback: {e}")
 
@@ -71,41 +88,83 @@ class DataCollector(Node):
         try:
             current_time = time.time()
             if self.record_flag:
-                self.get_logger().info("Recording RPM setpoint data")
-                self.rpm_setpoint.append(msg.data[2])
-                self.can_data.append(("nan", "nan", "nan"))
-                self.time_stamps.append(current_time)
-                self.get_logger().info(f"Received RPM message at {current_time}: {msg.data}")
-                self.save_data()  # Save data after processing RPM message
+                self.save_rpm_information(msg.data, current_time)
         except Exception as e:
             self.get_logger().error(f"Error in RPM callback: {e}")
 
+    def save_can_information(self, can_msg, current_time):
+        try:
+            msg_list = can_msg.split(",")
+            can_data = bytes.fromhex(msg_list[1])
+            command_id = int.from_bytes(bytes.fromhex(msg_list[0])[2:3], "big", signed=True)
+            motor_id = int.from_bytes(bytes.fromhex(msg_list[0])[3:4], "big", signed=True)
+
+            if (motor_id in motor_ids) and (command_id in command_ids):
+                self.get_logger().info("Recording CAN data")
+                motor_name = motor_ids[motor_id]
+                
+                entry = {
+                    'timestamp': current_time,
+                    'motor_id': motor_name,
+                    'rpm_setpoint': "nan",
+                    'rpm': "nan",
+                    'current': "nan",
+                    'battery': "nan",
+                }
+
+                if command_id == CAN_PACKET_STATUS:
+                    entry['current'] = int.from_bytes(can_data[4:6], "big", signed=True) / 10.0
+                    entry['rpm'] = int.from_bytes(can_data[0:4], "big", signed=True)
+
+                elif command_id == CAN_PACKET_STATUS_5:
+                    entry['battery'] = int.from_bytes(can_data[4:6], "big", signed=True) / 10.0
+
+                self.data_entries.append(entry)
+                self.save_data()
+
+        except Exception as e:
+            self.get_logger().error(f"Error extracting CAN message: {e}")
+
+    def save_rpm_information(self, rpm, current_time):
+        try:
+            self.get_logger().info("Recording RPM setpoint data")
+            for rpm_value in rpm:
+                entry = {
+                    'timestamp': current_time,
+                    'motor_id': "nan",
+                    'rpm_setpoint': rpm_value,
+                    'rpm': "nan",
+                    'current': "nan",
+                    'battery': "nan",
+                }
+                self.data_entries.append(entry)
+            self.save_data()
+        except Exception as e:
+            self.get_logger().error(f"Error extracting RPM message: {e}")
+
     def save_data(self):
-        if not self.time_stamps:
+        if not self.data_entries:
             self.get_logger().warning("No data to save.")
             return 
 
         with open(self.csv_filename, 'a', newline='') as csvfile:
-            fieldnames = ['timestamp', 'rpm_setpoint', 'rpm', 'current', 'battery']
+            fieldnames = ['timestamp', 'motor_id', 'rpm_setpoint', 'rpm', 'current', 'battery']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
             # Write data
-            for timestamp, rpm_setpoint, can_data in zip(self.time_stamps, self.rpm_setpoint, self.can_data):
-                rpm_can, current_can, battery_can = can_data
-                writer.writerow({'timestamp': timestamp, 'rpm_setpoint': rpm_setpoint, 'rpm': rpm_can, 'current': current_can, 'battery': battery_can})
+            for entry in self.data_entries:
+                writer.writerow(entry)
 
             self.get_logger().info(f"Data saved to {self.csv_filename}")
 
-        # Clear lists after saving
-        self.time_stamps.clear()
-        self.can_data.clear()       
-        self.rpm_setpoint.clear()
+        # Clear the data entries after saving
+        self.data_entries.clear()
 
     def record_service_callback(self, request, response):
         self.record_flag = not self.record_flag
         self.get_logger().info(f"Recording {'started' if self.record_flag else 'stopped'}.")
         return response
-
+    
 def main(args=None):
     rclpy.init(args=args)
     data_collector = DataCollector()
@@ -119,51 +178,6 @@ def main(args=None):
     data_collector.save_data()  # Ensure data is saved before shutting down
     data_collector.destroy_node()
     rclpy.shutdown()
-
-def extract_rpm_can(can_msg):
-    try:
-        msg_list = can_msg.split(",")
-        can_data = bytes.fromhex(msg_list[1])
-        command_id = int.from_bytes(bytes.fromhex(msg_list[0])[2:3], "big", signed=True)
-        motor_id = int.from_bytes(bytes.fromhex(msg_list[0])[3:4], "big", signed=True)
-
-        if motor_id == 0x0001 and command_id == 9:
-            rpm = int.from_bytes(can_data[0:4], "big", signed=True)
-            return rpm
-    except Exception as e:
-        print(f"Error extracting RPM from CAN message: {e}")
-
-    return "nan"
-
-def extract_current_can(can_msg):
-    try:
-        msg_list = can_msg.split(",")
-        can_data = bytes.fromhex(msg_list[1])
-        command_id = int.from_bytes(bytes.fromhex(msg_list[0])[2:3], "big", signed=True)
-        motor_id = int.from_bytes(bytes.fromhex(msg_list[0])[3:4], "big", signed=True)
-
-        if motor_id == 0x0001 and command_id == 9:
-            current = int.from_bytes(can_data[4:6], "big", signed=True) / 10.0
-            return current
-    except Exception as e:
-        print(f"Error extracting current from CAN message: {e}")
-
-    return "nan"
-
-def extract_battery_can(can_msg):
-    try:
-        msg_list = can_msg.split(",")
-        can_data = bytes.fromhex(msg_list[1])
-        command_id = int.from_bytes(bytes.fromhex(msg_list[0])[2:3], "big", signed=True)
-        motor_id = int.from_bytes(bytes.fromhex(msg_list[0])[3:4], "big", signed=True)
-
-        if motor_id == 0x0001 and command_id == 27:
-            battery = int.from_bytes(can_data[4:6], "big", signed=True) / 10.0
-            return battery
-    except Exception as e:
-        print(f"Error extracting battery voltage from CAN message: {e}")
-
-    return "nan"
 
 if __name__ == '__main__':
     main()
